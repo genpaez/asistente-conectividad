73dd892a90254a6abda7738ae0dacd2ba07f9223
Eymer Páez <genpaez@gmail.com>
2018-11-13 06:33:27 -0500

    Initial commit

---
+++ src/main/java/com/oesia/model/ServiciosRepository.java
@@ -0,0 +1,18 @@
+package com.oesia.model;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+	
+	@Repository
+	public interface ServiciosRepository extends CrudRepository<Sedes, Integer>{
+		//servicio_ds, servicio_sede, servicio_tipo
+	//	SELECT ciudad_id, ciudad_nombre FROM tb_ciudades JOIN tb_sedes WHERE tb_sedes.sede_cliente = ?1 GROUP BY ciudad_nombre
+		@Query(value ="SELECT servpnip_id, servpnip_tipo_enlace FROM tb_servicio_vpnip JOIN tb_servicios WHERE servicio_sede = ?1 GROUP BY servpnip_tipo_enlace", nativeQuery = true)
+		List<?> findServicioSede(int servicio_sede); 
+	}
+
+
+
