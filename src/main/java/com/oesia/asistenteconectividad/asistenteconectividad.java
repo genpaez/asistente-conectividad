1581331475c08ec63eb811d22beea9336147e1bc
genpaez <genpaez@gmail.com>
2018-11-22 12:09:57 -0500

    ok
    ok

--- src/main/java/com/oesia/asistenteconectividad/asistenteconectividad.java
+++ src/main/java/com/oesia/asistenteconectividad/asistenteconectividad.java
@@ -3,7 +3,6 @@ package com.oesia.asistenteconectividad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
